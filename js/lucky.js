f4938f26e484631e13fc6cc7acda25cae65861d3
duytinh <duytinh.vn@gmail.com>
2025-07-26 16:29:36 +0700

    dsds

--- js/lucky.js
+++ js/lucky.js
@@ -1,20 +1,29 @@
-if (confirm("Game lucky number")) {
-    var s = prompt("Vui lòng nhập số của bạn từ 0 đến 99");
-    s = parseInt(s);
 
-    if ( s < 0 || s > 99) {
-        alert("Số bạn nhập không hợp lệ. Vui lòng nhập số từ 0 đến 99.");
-    } else {
-        var r = Math.random(); // return number -> duoble/float 0->1 vd 0.12791
-        r = r * 100; 
-        r = parseInt(r);
+var luckyNumber = Math.random();
+luckyNumber = luckyNumber * 100;
+luckyNumber = parseInt(luckyNumber);
 
-        if (s === r) {
-            alert("Chúc mừng! Bạn đã đoán đúng số may mắn: " + s);
-        } else {
-            alert("Rất tiếc! Bạn đã đoán sai. Số may mắn là: " + r);
-        }
-    }
+
+const sodudoan = prompt("Nhập số bạn dự đoán (0 - 99):");
+sodudoan = parseInt(sodudoan);
+
+
+if (sodudoan < 0 || sodudoan > 99) {
+  alert("Số bạn nhập không hợp lệ! Vui lòng nhập số từ 0 đến 99.");
 } else {
-    alert("Bạn hãy tham gia trò chơi.");
+  const sotiendautu = prompt("Nhập số tiền bạn muốn đầu tư (đơn vị: VND):");
+  sotiendautu = parseFloat(sotiendautu);
+
+  if (sotiendautu || sotiendautu) <= 0 {
+    alert("Số tiền đầu tư không hợp lệ!");
+  } else {
+    if (sodudoan === luckyNumber) {
+      var tienthuong = sotiendautu * 70;
+      alert("Chúc mừng! Bạn đã đoán đúng số may mắn" +luckyNumber);
+      alert("Số tiền bạn thắng được là" +tienthuong);
+    } else {
+      alert("Rất tiếc! Số may mắn là" +luckyNumber);
+      alert("bạn đã đoán là" +sodudoan);
+    }
+  }
 }
